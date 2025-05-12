34c83380a6aa9b799d5e333db8bf6488516f8dcc
David Muñoz Peña <90482168+DavidMunoz25@users.noreply.github.com>
2025-05-12 16:24:53 -0600

    Update Main.java

--- src/main/java/com/mantenimiento/morado/Main.java
+++ src/main/java/com/mantenimiento/morado/Main.java
@@ -1,10 +1,13 @@
 package com.mantenimiento.morado;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import com.mantenimiento.morado.code.counter.FileFormatter;
 import com.mantenimiento.morado.code.counter.SourceFileAnalyzer;
+import com.mantenimiento.morado.code.model.SourceFile;
 import com.mantenimiento.morado.util.FileHelper;
 
 public class Main {
@@ -30,6 +33,35 @@ public class Main {
 
         SourceFileAnalyzer analyzer = new SourceFileAnalyzer(directoryPaths);
         analyzer.analyzePath();
+
+        try {
+            String newVersionPath = directoryPaths.get(1);
+            File folder = new File(newVersionPath);
+            File[] files = folder.listFiles((dir, name) -> name.endsWith(".java"));
+
+            if (files != null) {
+                FileFormatter formatter = new FileFormatter();
+
+                for (File file : files) {
+                    String filename = file.getName();
+                    String filePath = file.getAbsolutePath();
+
+                    // Intenta encontrar archivo correspondiente en oldVersion
+                    String oldVersionPath = directoryPaths.get(0) + "/" + filename;
+                    File oldFileCandidate = new File(oldVersionPath);
+                    SourceFile oldFile = oldFileCandidate.exists() ? new SourceFile(filename, oldVersionPath) : null;
+
+                    SourceFile newFile = new SourceFile(filename, filePath);
+                    formatter.formatFile(oldFile, newFile);
+                    System.out.println("Formateado: " + filename);
+                }
+            } else {
+                System.out.println("No se encontraron archivos .java en la carpeta newVersion.");
+            }
+        } catch (Exception e) {
+            System.err.println("Error al formatear archivos: " + e.getMessage());
+        }
+        
     }
 
 }
